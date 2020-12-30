4644631b2a08a88833c15b7980aa66939e7e1094
Saloni-hub <pandeysaloni4julya@gmail.com>
2020-12-30 10:48:14 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-const {newtonLeadeboardModel} = require('./connector')
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
-const posts = require('./data');
 const port = 8080
 app.use(express.urlencoded());
 
-// Parse JSON bodies (as sent by API clients)m
+// Parse JSON bodies (as sent by API clients)
 app.use(express.json());
-
+const { data } = require('./data')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
-
 // your code goes here
-app.get('/topRankings', async(req,res) => {
-    res.send(await newtonLeadeboardModel.find().skip(sanitize(req.query.offset, 0)).limit(sanitize(req.query.limit , 20)));
+
+app.get("/topRankings", async (req,res)=>{
+    const offset=getValue(req.query.offset,0);
+    const limit=getValue(req.query.limit,20) +offset;
+    const dataToSend=await data.slice(offset,limit);
+    console.log(dataToSend);
+    res.send(dataToSend);
 });
 
-const sanitize = (value, defaultValue) => {
-    if(value === null || value === undefined || isNaN(Number(value))) {
-        return defaultValue;
+const getValue = (value, defaultValue) => {
+    if (value === null || value === undefined || isNaN(Number(value))) {
+      return defaultValue;
     }
     return Number(value);
-}
-
-
+  };
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
 module.exports = app;
